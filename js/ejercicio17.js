3e394bee199d1cc70303a16490a1806ff25ad644
Agostina <lozagostina@gmail.com>
2022-07-13 22:59:05 -0300

    agrego ejercicio 17

---
+++ js/ejercicio17.js
@@ -0,0 +1,13 @@
+//pedir texto
+//mostrar primera vocal
+
+let frase= prompt('Ingresar texto');
+frase=frase.toLocaleLowerCase();
+
+for(let i=0; i < frase.length; i++){
+    if(frase.charAt(i) === 'a' || frase.charAt(i) === 'e' || frase.charAt(i) === 'i' || 
+frase.charAt(i) === 'o' || frase.charAt(i) === 'u') {
+    document.write(frase.charAt(i))
+    frase++
+}
+}
