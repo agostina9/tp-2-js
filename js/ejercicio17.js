01270954154aedf22a1405299ecf1b8a8f60d3e6
Agostina <lozagostina@gmail.com>
2022-07-13 23:00:00 -0300

    agrego ejercicio 17

--- js/ejercicio17.js
+++ js/ejercicio17.js
@@ -7,7 +7,7 @@ frase=frase.toLocaleLowerCase();
 for(let i=0; i < frase.length; i++){
     if(frase.charAt(i) === 'a' || frase.charAt(i) === 'e' || frase.charAt(i) === 'i' || 
 frase.charAt(i) === 'o' || frase.charAt(i) === 'u') {
-    document.write(frase.charAt(i))
+    document.write('La primera vocal del texto es:' + frase.charAt(i))
     frase++
 }
 }
