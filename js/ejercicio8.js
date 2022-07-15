59f36adf875b3911dfc7dad325a3e91e396785bc
Agostina <lozagostina@gmail.com>
2022-07-14 21:27:57 -0300

    agrego ejercicio 8

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,10 +1,13 @@
-/*let numerousuario = parseInt(prompt('Ingresar un numero menor a 50'));
+let numerousuario = parseInt(prompt('Ingresar un numero menor a 50'));
 console.log(numerousuario);
 
+let numero=0
+let repeticion=1
+
 if( numerousuario > 0 && numerousuario<=50){
-    for(let numero = numerousuario; numero >= 1; numero++){
-        for(let repeticion = numero; repeticion=numero + numerousuario; repeticion++){
-            document.write(numero)
+    for( numero; numero <= numerousuario; numero++){
+        for( repeticion = 1; repeticion<=numero; repeticion++){
+            document.write(repeticion)
         }
         document.write('<br>')
     }
